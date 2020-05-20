9a15e6363d4b2dfb09e7c777ba7604a82d5c181c
Lauren Barry <laurenbarry00@gmail.com>
2020-05-19 21:00:05 -0700

    Added error handling and better alert text

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -6,7 +6,11 @@ class Popup extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // alert string, and whether it should have the "error" class
       alertText: "",
+      alertIsError: false,
+
+      // toggle advanced menu
       showAdvanced: false
     }
     this.toggleAdvanced = this.toggleAdvanced.bind(this);
@@ -20,14 +24,16 @@ class Popup extends Component {
   }
 
   handleStash() {
-    const _this = this;
+    const _this = this; // for scoping inside callbacks
 
     // query all tabs
     chrome.tabs.query({}, function(results) {
-      let tabUrls = [];
+      let tabUrls = []; // list of all open tabs
+      let alertString; // error/confirmation request
 
       // get all open tabs' urls and indices
       results.forEach(function(tab) {
+        // save url, index, and active status of each tab
         tabUrls.push({
           url: tab.url,
           index: tab.index,
@@ -36,12 +42,16 @@ class Popup extends Component {
       });
 
       // store in chrome storage as json string
-      //const openTabs = JSON.stringify(tabUrls);
-      chrome.storage.local.set({tabStash: tabUrls});
-
-      // update alert text w/ how many tabs were stored
-      const alertString = tabUrls.length + " tabs stashed.";
-      _this.setState({alertText: alertString});
+      chrome.storage.local.set({tabStash: tabUrls}, function() {
+        // set error text if error occurred
+        if (chrome.runtime.lastError) {
+          alertString = "An error occurred, tabs failed to be stashed."
+          _this.setState({ alertText: alertString, alertIsError: true });
+        } else { // set confirmation text if success
+          alertString = tabUrls.length + " tabs stashed.";
+          _this.setState({ alertText: alertString, alertIsError: false });
+        }
+      });
     });
   }
 
@@ -50,18 +60,27 @@ class Popup extends Component {
 
     // retrieve stashed tabs from storage
     chrome.storage.local.get(["tabStash"], function(result) {
+      let alertString;
+
+      // set alert message/status if error
+      if (chrome.runtime.lastError) {
+        alertString = "An error occurred, stash could not be applied.";
+        _this.setState({ alertText: alertString, alertIsError: true });
+        return;
+      }
+
       // handle empty stash
       if (Object.keys(result).length === 0) {
         // display empty stash message
         const alertString = "Cannot apply, stash is empty.";
-        _this.setState({alertText: alertString});
+        _this.setState({ alertText: alertString, alertIsError: true });
         return;
       }
 
       // get list of tabs
       const stashTabs = result.tabStash;
       stashTabs.forEach(function(tab) {
-        // create new tab w/ index
+        // create new tab w/ index and active status
         chrome.tabs.create({
           url: tab.url,
           index: tab.index,
@@ -72,16 +91,39 @@ class Popup extends Component {
   }
 
   handleClear() {
+    const _this = this;
+
     // remove stash from storage
-    chrome.storage.local.remove("tabStash");
-    // update alert text to confirm stash clear
-    this.setState({alertText: "Stash cleared."});
+    chrome.storage.local.remove("tabStash", function() {
+      let alertString;
+
+      // handle error if it occurs
+      if (chrome.runtime.lastError) {
+        alertString = "An error occurred, stash could not be cleared.";
+        _this.setState({ alertText: alertString, alertIsError: true });
+        return;
+      }
+
+      // update alert text to confirm stash clear
+      _this.setState({ alertText: "Stash cleared.", alertIsError: false });
+    });
   }
 
   render() {
-    const alertText = this.state.alertText;
+    const _this = this;
+
+    // create alert element with alert text, error class(?)
+    let alertMessage;
+   if(_this.state.alertIsError) {
+      alertMessage = <p className="alert-text error">{this.state.alertText}</p>
+    } else {
+      alertMessage = <p className="alert-text">{this.state.alertText}</p>
+    }
+
+    // open/close menu button
     const menuIcon = this.state.showAdvanced ? "\u2715 Close" : "\u2630 Menu";
 
+    // show menu elements if toggled on
     const advancedMenu = this.state.showAdvanced ?
       <div className="advanced-menu">
         <button id="clear-button" title="Clear stashed tabs" onClick={this.handleClear}>Clear Stash</button>
@@ -91,12 +133,12 @@ class Popup extends Component {
       <div className="popup">
         <h1 id="title">TabStash</h1>
         <img src="./img/icon128.png" alt="TabStash icon" ></img>
-        <p className="alert-text">{alertText}</p>
+        {alertMessage}
         <div className="main-buttons-container">
           <button id="stash-button" title="Stash current tabs" onClick={this.handleStash}>Stash</button>
           <button id="apply-button" title="Apply stashed tabs" onClick={this.handleApply}>Apply</button>
         </div>
-        <div id="adv-menu-icon" onClick={this.toggleAdvanced}>{menuIcon}</div>
+        <div id="adv-menu-button" onClick={this.toggleAdvanced}>{menuIcon}</div>
         {advancedMenu}
       </div>
     );
